f03f15c7682c1bb0da6dc1a9070fb306b31cba71
deepto-reza <90522806+deepto-reza@users.noreply.github.com>
2023-03-26 23:47:05 +0600

    Nav Bar Uploaded

--- audioshell-Frontend/src/Components/Shared/Navbar.js
+++ audioshell-Frontend/src/Components/Shared/Navbar.js
@@ -3,8 +3,64 @@ import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-black bg-gradient py-4">
-      <NavLink style={{ textDecoration: "none" }}>
+    <div className="navbar bg-[#5865F2] text-white">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="./dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="./addnew">Add new</Link>
+            </li>
+            <li>
+              <Link to="./login">Login</Link>
+            </li>
+            <li>
+              <Link to="./signup">Signup</Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          Audioshell
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="./dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="./addnew">Add new</Link>
+          </li>
+          <li>
+            <Link to="./Login">Login</Link>
+          </li>
+          <li>
+            <Link to="./signup">Signup</Link>
+          </li>
+        </ul>
+      </div>
+      {/* <NavLink style={{ textDecoration: "none" }}>
         <Link className="mx-2 text-white text-decoration-none" to="/">
           Home
         </Link>
@@ -14,10 +70,13 @@ const Navbar = () => {
         <Link className="mx-2 text-white text-decoration-none" to="/signup">
           Signup
         </Link>
+        <Link className="mx-2 text-white text-decoration-none" to="/dashboard">
+          Dashboard
+        </Link>
         <Link className="mx-2 text-white text-decoration-none" to="/addnew">
           Add New
         </Link>
-      </NavLink>
+      </NavLink> */}
     </div>
   );
 };
